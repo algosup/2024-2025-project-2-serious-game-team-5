916e7a17db1fa826e77d6b1f65162b6055ce20c0
Maxime CARON <145995231+MaximeAlgosup@users.noreply.github.com>
2024-12-09 17:02:25 +0100

    left click handle

--- brought-back-city/GDScript/GameScript/InterfaceScript/Marker.gd
+++ brought-back-city/GDScript/GameScript/InterfaceScript/Marker.gd
@@ -6,10 +6,10 @@ func _ready() :
 	marker = get_node("/root/World/Marker")
 
 
-	
-
-
 func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	var posX = round(event_position.x) - 0.5
 	var posY = round(event_position.z) - 0.5
 	marker.transform.origin = Vector3(posX, 0, posY)
+	
+	if Input.is_action_just_pressed("left_click"):
+		print("Ma GROSSE BITE DE NEEEEEEGRE")
