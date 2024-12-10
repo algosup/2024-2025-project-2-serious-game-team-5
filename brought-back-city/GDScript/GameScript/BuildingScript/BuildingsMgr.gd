03a369990e9a86f1bea130c7c89f7ffe90b833a6
Alexis Lasselin <alexis.lasselin@algosup.com>
2024-12-10 10:26:19 +0100

    update grid map size

--- brought-back-city/GDScript/GameScript/BuildingScript/BuildingsMgr.gd
+++ brought-back-city/GDScript/GameScript/BuildingScript/BuildingsMgr.gd
@@ -47,7 +47,7 @@ var buildingId = 1
 # Building creation
 func CreateBuilding(pos: Vector3):
 	# Load the building resource
-	var building_res = load(small_house)
+	var building_res = load(skyscraper)
 	var building = building_res.instantiate()
 	
 	building.position = pos  # Use 'position' to set the location in 3D
