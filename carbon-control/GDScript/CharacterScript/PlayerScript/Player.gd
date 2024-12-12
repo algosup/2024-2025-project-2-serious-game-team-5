2c2793630b7d8a7dd68d9c68985a8eba85e152b6
Pierre Gorin <91249863+Pierre2103@users.noreply.github.com>
2024-12-12 16:45:59 +0100

    New Movements
    - W to go forward
    - S to go backward
    - A to go left
    - D to go right
    - R to zoom in
    - F to zoom out
    
    Change the minimum zoom distance
    [old = 10 -> new = 5]
    
    Change the maximum zoom distance
    [old = 150 -> new = 100]
    
    Slow the zoom_speed
    [old = 10 -> new = 5]
    
    Note: zooming using the keyboard is multiplied by 6 to make it faster and smoother

--- carbon-control/GDScript/CharacterScript/PlayerScript/Player.gd
+++ carbon-control/GDScript/CharacterScript/PlayerScript/Player.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 var mouse_sensitivity := 0.05
 var moving_camera_input := Vector3()
-var zoom_speed := 10
+var zoom_speed := 5
 var mouse = Vector2()
 var last_hovered_building = null
 const distance = 1000
@@ -16,12 +16,29 @@ func _process(_delta: float) -> void:
 	# Allow the player to press 'escape' to exit the window
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		
+
+	# Handle camera movements with WASD keys
+	if Input.is_key_pressed(KEY_W):
+		translate(Vector3(0, 0, -0.2))
+	if Input.is_key_pressed(KEY_S):
+		translate(Vector3(0, 0, 0.2))
+	if Input.is_key_pressed(KEY_A):
+		translate(Vector3(-0.2, 0, 0))
+	if Input.is_key_pressed(KEY_D):
+		translate(Vector3(0.2, 0, 0))
+
+	# Handle camera zoom with R and F keys
+	if Input.is_key_pressed(KEY_R):
+		player_camera.size = clamp(player_camera.size - (zoom_speed * 6) * _delta, 5.0, 100.0)
+	if Input.is_key_pressed(KEY_F):
+		player_camera.size = clamp(player_camera.size + (zoom_speed * 6) * _delta, 5.0, 100.0)
+
+	# Movement using right mouse button
 	if Input.is_action_pressed("moving_camera"):
 		translate(moving_camera_input)
-		
+
 # This function will handle both camera movement and camera zoom.
-func _unhandled_input(event: InputEvent) -> void:		
+func _unhandled_input(event: InputEvent) -> void:
 	# Camera movement while holding right mouse button
 	if event is InputEventMouseMotion:
 		if Input.is_action_pressed("moving_camera") and Input.get_mouse_mode() == Input.MOUSE_MODE_CONFINED:
@@ -30,15 +47,15 @@ func _unhandled_input(event: InputEvent) -> void:
 		elif Input.is_mouse_button_pressed(MOUSE_BUTTON_MIDDLE):
 			player_camera.rotation_degrees.y -= event.screen_relative.x * mouse_sensitivity  # Rotate horizontally
 			player_camera.rotation_degrees.x -= event.screen_relative.y * mouse_sensitivity  # Rotate vertically
-	
+
 	# Camera zoom-in (wheel down) and zoom-out (wheel up)
 	elif event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_WHEEL_UP && not(GlobalVariables.popped_up):
-			player_camera.size = clamp(player_camera.size - zoom_speed, 10.0, 150.0)
-		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN && not(GlobalVariables.popped_up):
-			player_camera.size = clamp(player_camera.size + zoom_speed, 10.0, 150.0)
-			
-# This function handle the interactions of the player with left click.			
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP and not(GlobalVariables.popped_up):
+			player_camera.size = clamp(player_camera.size - zoom_speed, 5.0, 100.0)
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN and not(GlobalVariables.popped_up):
+			player_camera.size = clamp(player_camera.size + zoom_speed, 5.0, 100.0)
+
+# This function handles the interactions of the player with left click.            
 func _input(event):
 	if event is InputEventMouseMotion:
 		mouse = event.position
@@ -46,7 +63,7 @@ func _input(event):
 		if Input.is_action_pressed("interact"):
 			_get_mouse_pos(mouse)
 
-# This function determine which building is clicked by the player
+# This function determines which building is clicked by the player
 func _get_mouse_pos(mouse: Vector2):
 	var space = get_world_3d().direct_space_state
 	var start = get_viewport().get_camera_3d().project_ray_origin(mouse)
@@ -55,7 +72,7 @@ func _get_mouse_pos(mouse: Vector2):
 	params.from = start
 	params.to = end
 	var result = space.intersect_ray(params)
-	
+
 	# Call the _hover_visible & _hover_hidden functions of Building.gd & Building_2.gd
 	if result and result.collider.has_method("_hover_visible"):
 		# Will hide the hover of the last hovered building
@@ -70,5 +87,5 @@ func _get_mouse_pos(mouse: Vector2):
 		for building in get_tree().get_nodes_in_group("buildings"):
 			if building.has_method("_hover_hidden"):
 				building.call("_hover_hidden")
-		# Will hide the hover effect for every buildings in "buildings" group
+		# Will hide the hover effect for every building in "buildings" group
 		last_hovered_building = null
